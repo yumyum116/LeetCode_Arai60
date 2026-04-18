ff240ba0b0323b07c7ba649d1b4809e17dfc189a
yumyum116 <yu24miura@gmail.com>
2026-04-18 15:16:05 +0000

    revised version.

---
+++ 111-minimum-depth-of-binary-tree/step2.py
@@ -0,0 +1,25 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+	def is_leaf(self, node: TreeNode) -> bool:
+		return node.left is None and node.right is None
+
+	def minDepth(self, root: Optional[TreeNode]) -> int:
+		if root is None:
+			return 0
+
+		node_and_level = deque([(root, 1)])
+
+		while node_and_level:
+			node, level = node_and_level.popleft()
+
+			if self.is_leaf(node):
+				return level
+
+			for child in (node.left, node.right):
+				if child is not None:
+					node_and_level.append((child, level + 1))
+
+		raise RuntimeError("unreachable")
